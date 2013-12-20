087373115d1200eba19aee8d97ff29228d5a83d5
John Gerick <gerick@kynes.mathematik.hu-berlin.de>
2013-12-20 13:37:40 +0100

    Komentare hinzugefuegt

--- NWE.R
+++ NWE.R
@@ -1,19 +1,29 @@
-#
-#
-# John Gerick und Fred Brockstedt hu-berlin 2013
-#
+## This Skript defines NWE for usage in Block3.R
+##
+## John Gerick und Fred Brockstedt hu-berlin 2013
+##
 
 
 
 ##  Nadaraya-Watson Estimator
 NWE <- function(matrix,KernFunction,bandWidth){
+## computes new matrix using Nararaya-Watson Estimation
+##
+##input:
+##	matrix - matrix with real
+##	KernFunction - a function
+##	nadWidth - a real
+##
+##output:
+##	nmat - matrix with real
+##
         nmat <- matrix
         n <- nrow(matrix)*ncol(matrix)
         z <- nrow(matrix)
         s <- ncol(matrix)
 
-        ymatrix <- matrix(rep(1:(2*s-1),(2*z-1)),ncol=(2*s-1),byrow=TRUE)
         xmatrix <- matrix(rep(1:(2*z-1),(2*s-1)),ncol=(2*s-1),byrow=FALSE)
+        ymatrix <- matrix(rep(1:(2*s-1),(2*z-1)),ncol=(2*s-1),byrow=TRUE)
 
         Kh <- sapply( ((z-xmatrix)/bandWidth) , KernFunction) * sapply( ((s-ymatrix)/bandWidth) , KernFunction)
         Kh <- matrix(Kh,ncol=(2*s-1))
@@ -23,5 +33,6 @@ NWE <- function(matrix,KernFunction,bandWidth){
                         nmat[zeil,spal] <- sum(matrix*Kh[(z+1-zeil):(2*z-zeil),(s+1-spal):(2*s-spal)] )/sum(Kh[(z+1-zeil):(2*z-zeil),(s+1-spal):(2*s-spal)] )
                 }
         }
+
         return(nmat)
 }
